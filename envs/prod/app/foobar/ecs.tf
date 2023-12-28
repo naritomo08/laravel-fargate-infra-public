92763c6ec46154c973cfe8637ea1c63dbcb19db6
naritomo08 <naritomo08@gmail.com>
2023-12-28 17:21:57 +0900

    upload

--- envs/prod/app/foobar/ecs.tf
+++ envs/prod/app/foobar/ecs.tf
@@ -1,11 +1,6 @@
 resource "aws_ecs_cluster" "this" {
   name = "${local.name_prefix}-${local.service_name}"
 
-  capacity_providers = [
-    "FARGATE",
-    "FARGATE_SPOT"
-  ]
-
   tags = {
     Name = "${local.name_prefix}-${local.service_name}"
   }
--- envs/prod/app/foobar/s3.tf
+++ envs/prod/app/foobar/s3.tf
@@ -2,15 +2,17 @@ resource "aws_s3_bucket" "env_file" {
   bucket = "terraform-state-${local.name_prefix}-${local.service_name}-env-file"
   force_destroy = true
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
   tags = {
     Name = "terraform-state-${local.name_prefix}-${local.service_name}-env-file"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "env_file" {
+  bucket = "terraform-state-${local.name_prefix}-${local.service_name}-env-file"
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
